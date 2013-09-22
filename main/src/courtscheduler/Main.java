28280de3e48ecf0073eba6327c116b660992f59c
Kyle Falconer <kfalconer@gmail.com>
2013-09-21 22:35:03 -0500

    Fixed SolverConfig loading

--- main/src/courtscheduler/Main.java
+++ main/src/courtscheduler/Main.java
@@ -18,7 +18,7 @@ public class Main {
 
         // http://docs.jboss.org/drools/release/5.5.0.Final/drools-planner-docs/html_single/index.html#d0e1961
         XmlSolverFactory solverFactory = new XmlSolverFactory();
-        solverFactory.configure("SolverConfig.xml");
+        solverFactory.configure("/courtscheduler/solver/SolverConfig.xml");
         SolverConfig solverConfig = solverFactory.getSolverConfig();
         solverConfig.getTerminationConfig().setMaximumMinutesSpend(1000L);  // FIXME: get this from the config file
         Solver solver = solverConfig.buildSolver();
