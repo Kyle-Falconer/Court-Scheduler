d9a248574eafa90d64ff6967dabb8219d4dc19ee
Stephen Kaysen <Kaysen7@live.missouristate.edu>
2013-09-21 22:52:20 -0500

    created solving file structure

---
+++ main/src/courtscheduler/domain/MatchAssignment.java
@@ -0,0 +1,11 @@
+package courtscheduler.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 9/21/13
+ * Time: 10:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MatchAssignment {
+}
---
+++ main/src/courtscheduler/solver/move/MatchChangeMoveFactory.java
@@ -0,0 +1,11 @@
+package courtscheduler.solver.move;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 9/21/13
+ * Time: 10:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class MatchChangeMoveFactory {
+}
