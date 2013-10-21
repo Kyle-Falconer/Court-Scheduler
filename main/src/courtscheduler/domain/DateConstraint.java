3e4e8166b387c62a40cb6db9bb028843828e5d1d
CJ <done245@live.missouristate.edu>
2013-10-21 09:16:45 -0500

    Reworked DateConstraint. It now utalizes a boolean[][] with default of 365x24. Methods for parsing have been changed from wrappers for adding to "find" functions that return the int, or int[] needed for the add functions.

--- main/src/courtscheduler/domain/DateConstraint.java
+++ main/src/courtscheduler/domain/DateConstraint.java
@@ -13,55 +13,126 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class DateConstraint extends Constraint{
-    private List<MatchDate> restrictedDates;
+    private boolean restrictedDates[][];
 
-    public List<MatchDate> getRestrictedDates(){
+    //constructors
+    public DateConstraint(){
+        restrictedDates= new boolean[365][24];
+    }
+    public DateConstraint(int days, int times){
+        restrictedDates= new boolean[days][times];
+    }
+
+    //get functions
+    public boolean[][] getRestrictedDates(){
         return this.restrictedDates;
     }
-    public void setRestrictedDates(List<MatchDate> badDates){
+
+
+
+    //set functions
+    public void setRestrictedDates(boolean[][] badDates){
         this.restrictedDates=badDates;
     }
-    public void addRestrictedDate(MatchDate badDate){
-        this.restrictedDates.add(badDate);
+
+
+    //add functions
+    //day/general adding
+    public void addRestrictedDate(int day, boolean[] times){
+        for(int i=0;i<this.restrictedDates.length;i++){
+            //and if true=ok, or if false=ok
+            this.restrictedDates[day][i]= (times[i]&&this.restrictedDates[day][i]);
+        }
     }
-    public void addRestrictedDate(Calendar badDate){
-        MatchDate bDate = new MatchDate();
-        bDate.setCal(badDate);
-		bDate.setDayOfWeek(getDayOfWeek(badDate));
-        this.restrictedDates.add(bDate);
+    //specific slot add
+    public void addRestrictedTime(int day, int time){
+        this.restrictedDates[day][time]=false;
     }
-    public void addRestrictedDate(String badDate){
+
+    //wrappers for other input types
+    // no time
+    public void addRestrictedDate(int day){
+        boolean[] noTimes  =new boolean[this.restrictedDates.length];
+        for(int i=0; i<noTimes.length;i++){
+            noTimes[i]=false;
+        }
+        this.addRestrictedDate(day,noTimes);
+    }
+    //no day
+    public void addRestrictedTimes(boolean[] times){
+        for(int i=0; i<this.restrictedDates.length;i++){
+            this.addRestrictedDate(i,times);
+        }
+    }
+    //array of days
+    public void addRestrictedDates(int days[], boolean[] times){
+        for(int i=0;i<days.length;i++){
+            this.addRestrictedDate(days[i],times);
+        }
+    }
+
+    //conversion methods for times
+    //int[] (0-24 with 1=1:00, size should not exceed 24, but need not be in order or full) ->boolean[]
+    public boolean[] makeTimeArray(int[] times){
+        boolean[] timeArray = new boolean[this.restrictedDates[0].length];
+        for(int i=0;i<times.length;i++){
+            timeArray[times[i]]=false;
+        }
+        return timeArray;
+    }
+    //start/end times -> boolean[]
+    public boolean[] makeTimeArray(int startTime, int endTime){
+        int[] times= new int[endTime-startTime];
+        for(int i=0;i<(endTime-startTime);i++){
+            times[i]=startTime+i;
+        }
+        return makeTimeArray(times);
+    }
+
+    //conversion methods for days
+    //Calendar date ->int date
+    public int findDate(Calendar badDate){
+        return badDate.get(Calendar.DAY_OF_YEAR);
+    }
+
+    //matchDate -> int day (calls calendar find date)
+    public int findDate(MatchDate badDate){
+        return findDate(badDate.getCal());
+    }
+    //string date->int day (calls calendar find date)
+    public int getDate(String badDate){
         Calendar bCal = Calendar.getInstance();
         String[] bDate= badDate.split("/");
         bCal.set(Integer.valueOf(bDate[2]),Integer.valueOf(bDate[0]),Integer.valueOf(bDate[1]));
-        this.addRestrictedDate(bCal);
+        return findDate(bCal);
     }
-    public void addRestrictedDates(List<MatchDate> badDates){
+    //array of matchDate inputs -> array of int days
+    public int[] findDates(List<MatchDate> badDates){
+        int[] days= new int[badDates.size()];
         for(int i=0;i<badDates.size();i++){
-            addRestrictedDate(badDates.get(i));
+            days[i]=badDates.get(i).getCal().get(Calendar.DAY_OF_YEAR);
         }
-    }
-    public void addRestrictedDates(Calendar startDate, Calendar endDate){
-        MatchDate badDate = new MatchDate();
-        while(startDate.before(endDate)){
-            badDate.setCal(startDate);
-			badDate.setDayOfWeek(getDayOfWeek(startDate));
-            this.restrictedDates.add(badDate);
-            startDate.add(Calendar.DATE,1);
+        return days;
 
-        }
     }
-    public void addRestrictedDates(String startDate, String endDate){
+    //Calender range -> int[] days
+    public int[] findDateRange(Calendar startDate, Calendar endDate){
+        int dayCount=endDate.get(Calendar.DAY_OF_YEAR)-startDate.get(Calendar.DAY_OF_YEAR);
+        int[] days= new int[dayCount];
+        for(int i=0;i<dayCount;i++){
+            days[i]=startDate.get(Calendar.DAY_OF_YEAR);
+            startDate.add(Calendar.DATE,1);
+        }
+        return days;
+    }
+    //string range -> int[] days (calls calendar range)
+    public int[] findDateRange(String startDate, String endDate){
         String[] sDate= startDate.split("/");
         String[] eDate= endDate.split("/");
         Calendar sCal= Calendar.getInstance();
         sCal.set(Integer.valueOf(sDate[2]),Integer.valueOf(sDate[0]),Integer.valueOf(sDate[1]));
         Calendar eCal= Calendar.getInstance();
         eCal.set(Integer.valueOf(eDate[2]),Integer.valueOf(eDate[0]),Integer.valueOf(eDate[1]));
-        this.addRestrictedDates(sCal,eCal);
+        return this.findDateRange(sCal,eCal);
     }
-	private DayOfWeek getDayOfWeek(Calendar date) {
-		// DayOfWeek class is zero-indexed, and Java's Calendar is one-indexed
-		return DayOfWeek.values()[date.get(Calendar.DAY_OF_WEEK) - 1];
-	}
 }
